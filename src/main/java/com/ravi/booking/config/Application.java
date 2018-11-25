38b08b0e5adf2206f59a4e1422183ff789102ad5
Ravi Verma <raviver2695@gmail.com>
2018-11-25 10:43:22 +0530

    testing spring boot

---
+++ src/main/java/com/ravi/booking/config/Application.java
@@ -0,0 +1,30 @@
+package com.ravi.booking.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@ComponentScan(basePackages = "com.ravi.booking")
+@Import({BeanConfig.class, WebConfig.class})
+//@EnableWebMvc
+@EnableAutoConfiguration
+public class Application extends SpringBootServletInitializer {
+
+    private static Class applicationClass = Application.class;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+    }
+
+}
+
