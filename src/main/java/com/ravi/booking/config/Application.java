606b1c0c75ce9c985045634984f9c469b0a3e0ac
Ravi Verma <raviver2695@gmail.com>
2018-12-01 12:03:39 +0530

    datasource

--- src/main/java/com/ravi/booking/config/Application.java
+++ src/main/java/com/ravi/booking/config/Application.java
@@ -1,18 +1,50 @@
 package com.ravi.booking.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Map;
+
 @SpringBootApplication
 @Controller
 public class Application {
+    @Autowired
+    private DatabaseConfig databaseConfig;
     @RequestMapping("/")
     @ResponseBody
     public String hello() {
         return "Hello World";
+    }
+
+    @RequestMapping("/db")
+    String db(Map<String, Object> model) {
+        try (Connection connection = databaseConfig.dataSource().getConnection()) {
+            Statement stmt = connection.createStatement();
+            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+            stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+            ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+
+            ArrayList<String> output = new ArrayList<String>();
+            while (rs.next()) {
+                output.add("Read from DB: " + rs.getTimestamp("tick"));
+            }
+
+            model.put("records", output);
+            return "db";
+        } catch (Exception e) {
+            model.put("message", e.getMessage());
+            return "error";
+        }
     }
 
     public static void main(String[] args) {
