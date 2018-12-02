3f63ac12bfc2a0b7912905718e8d50f74169646b
Ravi Verma <raviver2695@gmail.com>
2018-12-02 23:14:21 +0530

    Swagger added

--- src/main/java/com/ravi/booking/controller/HomeController.java
+++ src/main/java/com/ravi/booking/controller/HomeController.java
@@ -1,6 +1,5 @@
 package com.ravi.booking.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,11 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-    @Value("${spring.datasource.url}")
-    String url;
+
     @GetMapping("/")
     @ResponseBody
     public String hone() {
-        return "Hello World\n"+url;
+        return "Hello World";
     }
 }
