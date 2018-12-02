4d6d420653c445bd2fed152e1bc9edce9b04348d
Ravi Verma <raviver2695@gmail.com>
2018-12-02 22:21:43 +0530

    pg url

--- src/main/java/com/ravi/booking/controller/HomeController.java
+++ src/main/java/com/ravi/booking/controller/HomeController.java
@@ -1,14 +1,18 @@
 package com.ravi.booking.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+
+    @Value("${spring.datasource.url}")
+    String url;
     @GetMapping("/")
     @ResponseBody
     public String hone() {
-        return "Hello World";
+        return "Hello World\n"+url;
     }
 }
